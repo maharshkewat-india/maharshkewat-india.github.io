9c160d335d4e50a5337ca633ba85c78948bd5aa2
Maharsh kewat <maharshkewat01@gmail.com>
2026-08-18 02:44:49 +0530

    Feat: Add profile photo and confirm next.config.js for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  // ... any other configurations you might have
+  /**
+   * Tell Next.js to generate a static site that can be hosted anywhere.
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
+  output: 'export',
+
+  /** Disable server-based image optimization. Next.js will export original images. */
+  images: { unoptimized: true },
+
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
